7c78572b6b151d56bf3220646209746cde0238f3
M. Mert KAN <mehmetmertkan7@gmail.com>
2021-09-28 22:16:55 +0300

    added links to navbar

---
+++ db/migrate/20210928185509_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.string :currency
+      t.string :name
+      t.float :price
+      t.float :sale
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
