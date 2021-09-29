61e6810289633874e1b0235089bd530434d72366
M. Mert KAN <mehmetmertkan7@gmail.com>
2021-09-29 16:04:17 +0300

    added currency dropdown and name validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
     belongs_to :category
+    validates :name, :presence => true
+    validate :nameWordCountMoreThanOne
+    validates :price, :presence => true
+    CURRENCY_TYPES = ["TL", "USD", "EUR"]
+
+  def nameWordCountMoreThanOne
+    errors[:widget] << "name should at least be two words" if name.split.size < 2
+  end
 end
