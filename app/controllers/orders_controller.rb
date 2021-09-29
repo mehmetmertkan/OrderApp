38a2060f3b14d5348490f2deeab6f84a365fe1a1
M. Mert KAN <mehmetmertkan7@gmail.com>
2021-09-29 13:53:53 +0300

    added categories

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -64,6 +64,6 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:currency, :name, :price, :sale)
+      params.require(:order).permit(:currency, :name, :price, :sale, :category_id)
     end
 end
